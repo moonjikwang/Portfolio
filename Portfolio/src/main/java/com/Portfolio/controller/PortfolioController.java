7331eebbce6adb7405c92d98c9cc9dae2caff6c6
moonjikwang <shjk0315@gmail.com>
2023-04-28 09:58:48 +0900

    style: theme-color 설정

--- Portfolio/src/main/java/com/Portfolio/controller/PortfolioController.java
+++ Portfolio/src/main/java/com/Portfolio/controller/PortfolioController.java
@@ -12,7 +12,6 @@ public class PortfolioController {
 	}
 	@GetMapping("index")
 	public void index() {
-		
 	}
 	
 }
--- Portfolio/src/main/java/com/Portfolio/service/MemberService.java
+++ Portfolio/src/main/java/com/Portfolio/service/MemberService.java
@@ -39,28 +39,20 @@ public class MemberService {
 			URL url = new URL(reqURL);
 
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
 
 			conn.setRequestMethod("POST");
 			conn.setDoOutput(true);
-			// POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
 
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
 			StringBuilder sb = new StringBuilder();
 			sb.append("grant_type=authorization_code");
-
-			sb.append("&client_id=7226e94e9966b51700e15e1823408600"); // REST_API키 본인이 발급받은 key 넣어주기
-			sb.append("&redirect_uri=https://tomcat.jikwang.net/Portfolio/oauth"); // REDIRECT_URI 본인이 설정한 주소 넣어주기
-
+			sb.append("&client_id=7226e94e9966b51700e15e1823408600");
+			sb.append("&redirect_uri=https://tomcat.jikwang.net/Portfolio/oauth");
 			sb.append("&code=" + authorize_code);
 			bw.write(sb.toString());
 			bw.flush();
-
-			// 결과 코드가 200이라면 성공
 			int responseCode = conn.getResponseCode();
 			System.out.println("responseCode : " + responseCode);
-
-			// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String line = "";
 			String result = "";
@@ -70,9 +62,7 @@ public class MemberService {
 			}
 			System.out.println("response body : " + result);
 
-			// jackson objectmapper 객체 생성
 			ObjectMapper objectMapper = new ObjectMapper();
-			// JSON String -> Map
 			Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
 			});
 
