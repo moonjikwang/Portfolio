ff4bd544910a1a76d61be2cd1991cb6ac7bc34ba
Baekseungyeon <devablight@gmail.com>
2023-05-03 16:08:48 +0900

    feat: SecurityConfig 충돌 분기 수정

--- Portfolio/src/main/java/com/Portfolio/config/SecurityConfig.java
+++ Portfolio/src/main/java/com/Portfolio/config/SecurityConfig.java
@@ -1,45 +1,18 @@
 package com.Portfolio.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.Portfolio.service.UserService;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final UserService userService;
-  
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.httpBasic().disable();
     httpSecurity.csrf().disable();
-    httpSecurity
-    			.formLogin()
-    				.loginPage("/login")
-    				.defaultSuccessUrl("/")
-    			.and()
-    			.logout()
-    				.logoutSuccessUrl("/login")
-    				.invalidateHttpSession(true)
-    				;
+    httpSecurity.headers().frameOptions().sameOrigin();
   }
-  
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-	  return new BCryptPasswordEncoder();
-  }
-  
-  
 }
